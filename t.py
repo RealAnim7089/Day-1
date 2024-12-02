b07145a46ccc210fb334caac28b91ca87bc7a4b0
RealAnim7089 <130187705+RealAnim7089@users.noreply.github.com>
2024-12-01 21:17:58 -0500

    Add files via upload

---
+++ t.py
@@ -0,0 +1,30 @@
+left = []
+right = []
+
+with open("day1_exercise.txt") as file:
+  for line in file:
+    l, r = line.split()
+    left.append(int(l))
+    right.append(int(r))
+
+def total_distance(array1, array2):   # Part 1
+  l1 = sorted(array1)
+  l2 = sorted(array2)
+
+  new_list = sorted(list(zip(l1,l2)))
+  empty_l = list()
+
+  for num1, num2 in new_list:
+    empty_l.append(abs(num1 - num2))
+
+  return sum(empty_l)
+
+def part2(array1, array2):
+  empty_l = []
+  new_list = list(zip(array1, array2))
+
+  for num1, num2 in new_list:
+    empty_l.append(num1 * array2.count(num1))
+  return sum(empty_l)
+
+part2(left,right)
